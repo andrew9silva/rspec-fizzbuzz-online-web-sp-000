e6bb75b1ee971c864c8bc3174f734d78cb6989c4
Andrew Silva <andrewsilva9@gmail.com>
2019-03-31 17:24:26 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzvuzz(int)
+  if int % 3 == 0
+    "Fizz"
+  elsif int % 5 == 0
+    "Buzz"
+  elsif in % 3 == 0 && 5 == 0
+    "Fizzbuzz"
+  else
+    nil
+  end
