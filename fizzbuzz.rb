61331df3360b525ce9bee139c0a52d03b5c52a19
Andrew Silva <andrewsilva9@gmail.com>
2019-03-31 17:07:18 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-describe "fizzbuzz" do
+def "fizzbuzz" do
   it 'returns "Fizz" when the number is divisible by 3' do
     fizz_3 = fizzbuzz(3)
     expect(fizz_3).to eq("Fizz")
