acc68b9e569df4595b05bba72027339e4e4cc69d
Andrew Silva <andrewsilva9@gmail.com>
2019-03-31 17:52:12 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,10 +3,10 @@
 def fizzbuzz(int)
   if int % 3 == 0 && % 5 == 0
     "Fizzbuzz"
-  elsif int % 5 == 0
-    "Buzz"
   elsif int % 3 == 0
     "Fizz"
+  elsif int % 5 == 0
+    "Buzz"
   else int % !(3 == 0 || 5 == 0)
     nil
   end
